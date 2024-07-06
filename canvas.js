4406c80bf6f0bbfc3f9eb807580d2d90cb2b7aed
John Flores <johnvincentflores2424@gmail.com>
2024-07-06 21:25:08 +0800

    animated and bouncing each other.

--- canvas.js
+++ canvas.js
@@ -28,14 +28,84 @@ var c = canvas.getContext('2d');
 // c.strokeStyle = 'blue';
 // c.stroke();
 
-for (var i = 0; i < 32; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
+// for (var i = 0; i < 32; i++) {
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
+//     var r = Math.random() * 256;
+//     var g = Math.random() * 256;
+//     var b = Math.random() * 256;
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI * 2, false);
+//     c.strokeStyle = `rgb(${r}, ${g}, ${b})`;
+//     c.stroke();   
+// }
+// var x = Math.random() * innerWidth;
+// var y = Math.random() * innerHeight;
+// var dx = (Math.random() - 0.5) * 8;
+// var dy = (Math.random() - 0.5) * 8;
+// var radius = 30;
+
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
     var r = Math.random() * 256;
     var g = Math.random() * 256;
     var b = Math.random() * 256;
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = `rgb(${r}, ${g}, ${b})`;
-    c.stroke();
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = `rgb(${r}, ${g}, ${b})`;
+        c.stroke();
+        c.fillStyle = 'black'
+        c.fill();
+    } 
+
+    this.update = function(){
+        if (this.x + this.radius > innerWidth || this.y - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+    
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
 }
+
+var circleArray = [];
+
+for(var i = 0; i < 100; i++){
+    var radius = 9;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.2);
+    var dy = (Math.random() - 0.2);
+
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+console.log(circleArray);
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    for(var i = 0 ; i < circleArray.length; i++){
+        circleArray[i].update();
+    }
+}
+animate();
+
+// This is something cool.
+// This might be our first step to adding to our calculator effect.
+// just need some kind of magnet to attract and form a number.
+// This is just JavaScript HTML canvas, imagine if we use CSS
+// We can add glows and stuff, maybe make the circle have range of sizes
+// or ranges of glow to them.
