bb0a19a01a3aa168b8208237a20630e8998a4fc7
John Flores <johnvincentflores2424@gmail.com>
2024-07-08 01:03:23 +0800

    No dynamic resizing

--- canvas.js
+++ canvas.js
@@ -73,11 +73,10 @@ window.addEventListener('resize', function(){
 
 });
 
+var circleArray = [];
 // We are using array to save each circles discription
 //  var radius = 9;this is way more optimize.
 for(var i = 0; i < 300; i++){
-    
-    var circleArray = [];
     var radius = Math.random() * 3 + 1;
     var x = Math.random() * (innerWidth - radius * 2) + radius;
     var y = Math.random() * (innerHeight - radius * 2) + radius;
@@ -133,20 +132,6 @@ function Circle(x, y, dx, dy, radius) {{}
 }
 
 
-
-// We are using array to save each circles discription
-//  var radius = 9;this is way more optimize.
-for(var i = 0; i < 300; i++){
-    
-    var circleArray = [];
-    var radius = Math.random() * 3 + 1;
-    var x = Math.random() * (innerWidth - radius * 2) + radius;
-    var y = Math.random() * (innerHeight - radius * 2) + radius;
-    var dx = (Math.random() - 0.5 );
-    var dy = (Math.random() - 0.5);
-
-    circleArray.push(new Circle(x, y, dx, dy, radius));
-    }
 
 
 console.log(circleArray);
